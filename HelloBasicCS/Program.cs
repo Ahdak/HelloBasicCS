da72d36a7c587e46edee1885f25c868e41b6a978
Ahmed DAMMAK <ahmed.dammak.net@gmail.com>
2022-02-21 16:53:07 +0100

    Creating project

---
+++ HelloBasicCS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloBasicCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+        }
+    }
+}
